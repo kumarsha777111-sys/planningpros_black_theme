bd6b79111f6de52dd3ee055f122a2e016394c2a5
Kumar Shanu <kumar.shanu@opalina.in>
2025-09-11 18:11:13 +0530

    build fixes

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { Toaster } from '@/components/ui/toaster';
 import Header from '@/components/Header';
 import Hero from '@/components/Hero';
 import Services from '@/components/Services';
-import About from '@/components/About';
+import About from '@/components/about';
 import Team from '@/components/Team';
 import Process from '@/components/Process';
 import Contact from '@/components/Contact';
